b10b9ec2b7c73c48cba1dcbe46cd2610c143b4ac
michaelfreyj <michaelfreyj@gmail.com>
2020-06-07 23:10:14 -0400

    update info

--- setup.py
+++ setup.py
@@ -4,41 +4,20 @@
 """
 pfg
 ----------
-
-
+Command line utility to quickly generate files from custom templates
 
 """
 
 from setuptools import setup, find_packages
-
-import pfg
 
 def read(filename):
     with open(filename) as f:
         return f.read()
 
 setup(
-    name='grip',
-    version='4.5.2',
-    description='Render local readme files before sending off to GitHub.',
-    long_description=__doc__,
-    author='Joe Esposito',
-    author_email='joe@joeyespo.com',
-    url='http://github.com/joeyespo/grip',
-    license='MIT',
-    platforms='any',
-    packages=find_packages(),
-    package_data={'grip': ['static/*.*', 'static/octicons/*', 'templates/*']},
-    install_requires=read('requirements.txt').splitlines(),
-    extras_require={'tests': read('requirements-test.txt').splitlines()},
-    zip_safe=False,
-    entry_points={'console_scripts': ['grip = grip:main']},
-)
-
-setup(
     name="pfg",
     version="0.0.1",
-    author="Michael J. Frey"
+    author="Michael J. Frey",
     author_email="michaelfreyj@gmail.com",
     description="Command line utility to quickly generate files from custom templates",
     long_description=read("README.md"),
@@ -47,11 +26,14 @@ setup(
     license='MIT',
     platforms='any',
     packages=find_packages(),
-    package_data={'pytemplate' : 'templates/*'}
+    package_data={'pytemplate' : ['templates/*']},
+    # install_requires=read('requirements.txt').splitlines(),
+    # extras_require={'tests': read('requirements-test.txt').splitlines()},
+    # entry_points={'console_scripts': ['grip = grip:main']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
